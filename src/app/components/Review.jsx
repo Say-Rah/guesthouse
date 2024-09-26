685f47f7d4a6aee298a0b0303353da7016cf449e
Say-Rah <officialsarahsylvester@gmail.com>
2024-09-26 20:01:00 +0100

    fixed:importing Image for the ommitted components redo

--- src/app/components/Review.jsx
+++ src/app/components/Review.jsx
@@ -1,37 +1,36 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Image from 'next/image'
+import Image from 'next/image';
 
 const testimonials = [
   {
     message:
-      "Our stay at Waikiki&lsquo;s Guest House was nothing short of spectacular. The ambiance was elegant, and the service was impeccable. Every detail was thoughtfully considered, making our visit both luxurious and comfortable. It&lsquo;s truly a gem in American Victorian. Highly recommended!",
+      "Our stay at Waikiki&rsquo;s Guest House was nothing short of spectacular. The ambiance was elegant, and the service was impeccable. Every detail was thoughtfully considered, making our visit both luxurious and comfortable. It&rsquo;s truly a gem in American Victorian. Highly recommended!",
     name: "Emily Thompson",
     occupation: "Actress",
     image: "/img/testimonial1.png",
   },
   {
     message:
-      "From the moment I walked through the doors, Waikiki&lsquo;s Guest House made me feel like royalty. The accommodations were stunning, and the staff went above and beyond to ensure I had a perfect stay. It&lsquo;s the ideal retreat for anyone seeking a blend of opulence and relaxation.",
+      "From the moment I walked through the doors, Waikiki&rsquo;s Guest House made me feel like royalty. The accommodations were stunning, and the staff went above and beyond to ensure I had a perfect stay. It&rsquo;s the ideal retreat for anyone seeking a blend of opulence and relaxation.",
     name: "Robert Gideon-el",
     occupation: "Financial Analyst",
     image: "/img/testimonial2.png",
   },
   {
     message:
-      "Waikiki&lsquo;s Guest House is an oasis of luxury. The sophisticated design and attentive service created a memorable experience. It&lsquo;s clear that every detail is crafted with care. If you&lsquo;re looking for the ultimate luxury escape, this guest house is a must-visit!",
+      "Waikiki&rsquo;s Guest House is an oasis of luxury. The sophisticated design and attentive service created a memorable experience. It&rsquo;s clear that every detail is crafted with care. If you&rsquo;re looking for the ultimate luxury escape, this guest house is a must-visit!",
     name: "Paul Sylvester",
     occupation: "Travel Blogger",
     image: "/img/testimonial3.JPG",
   },
   {
     message:
-      "Waikiki&lsquo;s Guest House offers a comfortable stay with cozy rooms and friendly service, perfect for travelers looking to enjoy a relaxing getaway. Its convenient location near local attractions and beaches adds to the charm, making it a great spot for both leisure and business stays. The property is clean, well-maintained, and provides essential amenities to ensure guests have a pleasant experience.",
-    name: " Sayrah Doe",
+      "Waikiki&rsquo;s Guest House offers a comfortable stay with cozy rooms and friendly service, perfect for travelers looking to enjoy a relaxing getaway. Its convenient location near local attractions and beaches adds to the charm, making it a great spot for both leisure and business stays. The property is clean, well-maintained, and provides essential amenities to ensure guests have a pleasant experience.",
+    name: "Sayrah Doe",
     occupation: "Business Executive",
     image: "/img/testimonial4.jpg",
   },
-
 ];
 
 const Review = () => {
@@ -62,7 +61,7 @@ const Review = () => {
         <div className="text-left mx-auto mb-10 max-w-lg">
           <h1 className="text-3xl font-bold mb-4">Our Clients Say!</h1>
           <p>
-            From the moment we arrived at Waikiki&lsquo;s Guest House, we were
+            From the moment we arrived at Waikiki&rsquo;s Guest House, we were
             immersed in luxury and exceptional service. The attention to detail
             and the elegance of the surroundings made our stay truly
             unforgettable.
@@ -82,7 +81,7 @@ const Review = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {testimonials.slice(currentIndex, currentIndex + 2).map((testimonial, index) => (
               <div key={index} className="bg-white p-6 text-left shadow-lg w-[80%] mx-auto rounded-lg">
-                <p className="text-lg italic mb-4">"{testimonial.message}"</p>
+                <p className="text-lg italic mb-4">&quot;{testimonial.message}&quot;</p>
                 <div className="flex items-center">
                   <Image
                     src={testimonial.image}
